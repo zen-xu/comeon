e4c2af57fca7096e5afd534fdb089ce99d4f1695
ZhengYu, Xu <zen-xu@outlook.com>
2020-03-08 19:54:19 +0800

    add get_typed_signature function

---
+++ comeon/utils.py
@@ -0,0 +1,32 @@
+import inspect
+
+from typing import Any
+from typing import Callable
+from typing import Dict
+
+from pydantic.typing import ForwardRef
+from pydantic.typing import evaluate_forwardref
+
+
+def get_typed_signature(call: Callable[..., Any]) -> inspect.Signature:
+    signature = inspect.signature(call)
+    globalns = getattr(call, "__globals__", {})
+    typed_params = [
+        inspect.Parameter(
+            name=param.name,
+            kind=param.kind,
+            default=param.default,
+            annotation=get_typed_annotation(param, globalns),
+        )
+        for param in signature.parameters.values()
+    ]
+    typed_signature = inspect.Signature(typed_params)
+    return typed_signature
+
+
+def get_typed_annotation(param: inspect.Parameter, globalns: Dict[str, Any]) -> Any:
+    annotation = param.annotation
+    if isinstance(annotation, str):
+        annotation = ForwardRef(annotation)
+        annotation = evaluate_forwardref(annotation, globalns, globalns)
+    return annotation
